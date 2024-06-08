405b52e3dedf1ea558dfca88930f3bde96504af4
Enrique Dlt <enrique_dj.tp@outlook.com>
2024-06-07 18:30:18 -0600

    adds learning about components and props

--- lessons/01-starting-project/src/App.jsx
+++ lessons/01-starting-project/src/App.jsx
@@ -1,5 +1,6 @@
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 function genRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
@@ -19,11 +20,34 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  );
+}
+
 function App() {
   return (
     <div>
       <Header></Header>
       <main>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="the core UI building block."
+              img={componentsImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
         <h2>Time to get started!</h2>
       </main>
     </div>
