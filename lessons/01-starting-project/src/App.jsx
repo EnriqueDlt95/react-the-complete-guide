d24f882c98bbc157aca1eb879172ac0b7d7fa696
Enrique Dlt <enrique_dj.tp@outlook.com>
2024-06-15 15:59:47 -0600

    last lesson

--- lessons/01-starting-project/src/App.jsx
+++ lessons/01-starting-project/src/App.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react"; // al the functions from react that start with the "use" are Hooks
+// the special thing about react hooks is the they are technically regular functions
+// but they must only be called inside of React component functions
+// or inside of other react hooks
+
 import Header from "./components/Header";
 import CoreConcept from "./components/CoreConcept";
+import TabButton from "./components/TabButton";
 import { CORE_CONCEPTS } from "./data";
 
 function App() {
+  // we always have to call the react hooks in the top level of a component not inside of any other function inside the component
+  const [selectedTopic, setSelectedTopic] = useState("Please click a Button");
+
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
+
   return (
     <div>
       <Header></Header>
@@ -10,6 +24,7 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
+            code
             <CoreConcept
               title={CORE_CONCEPTS[0].title}
               description={CORE_CONCEPTS[0].description}
@@ -23,6 +38,18 @@ function App() {
             where we get the information we can use the spread operator and we
             will get the same key values */}
           </ul>
+        </section>
+        <section id="examples">
+          {/* the menu tag can serves tio us to have a list ob buttons  */}
+          <menu>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
+          </menu>
+          {selectedTopic}
         </section>
         <h2>Time to get started!</h2>
       </main>
---
+++ lessons/01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,12 @@
+export default function TabButton({ children, onSelect }) {
+  return (
+    <li>
+      <button onClick={onSelect}>{children}</button>
+      {/* we need to send as a value the function */}
+    </li>
+  );
+}
+
+// Another convention we will see a lot on React that Props that should in the end received
+// a function that will be triggered up on an Event should start with "on" and this should be passed
+// to the event that will be actioned on the compound
